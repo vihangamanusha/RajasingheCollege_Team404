c4677251c0763854c78e97b835130fae052dcdab
Chamod pinimal <chamodpinimal@gmail.com>
2026-04-22 19:24:24 +0530

    updated index

--- LMS/backend/index.js
+++ LMS/backend/index.js
@@ -9,39 +9,27 @@ const authenticateToken = require('./middleware/authMiddleware');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001; // Changed default port to 5001
+const PORT =  5001; 
 
 // Middleware setup
 app.use(cors()); // Enable Cross-Origin Resource Sharing
 app.use(express.json()); // Parse incoming JSON requests
 
-// MongoDB connection setup
-const dbUsername = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbCluster = process.env.DB_CLUSTER;
-const dbName = process.env.DB_NAME;
-
-const connectionString = `mongodb+srv://${dbUsername}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
+const connectionString = process.env.MONGO_URI;
 
 // Connect to MongoDB
-//mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-//  .then(() => console.log('Connected to MongoDB'))
-//    .catch(err => console.error('Failed to connect to MongoDB', err));
+mongoose.connect(connectionString).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+}
+);
 
-// Define a simple route
 app.get('/', (req, res) => {
   res.send('Rajasinghe College LMS Backend is running');
 });
 
-// Import routes
-const adminRoutes = require('./roles/admin/adminRoutes');
-const studentRoutes = require('./roles/student/studentRoutes');
-const teacherRoutes = require('./roles/teacher/teacherRoutes');
 
-// Protect and use routes
-app.use('/admin', authenticateToken, adminRoutes);
-app.use('/student', authenticateToken, studentRoutes);
-app.use('/teacher', authenticateToken, teacherRoutes);
 
 // Start the server
 app.listen(PORT, () => {
