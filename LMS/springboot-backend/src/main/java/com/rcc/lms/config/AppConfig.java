280d01f72e4743b0203ba901c8ec4ebe6117ca79
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-03 10:42:12 +0530

    Update AppConfig.java

--- LMS/springboot-backend/src/main/java/com/rcc/lms/config/AppConfig.java
+++ LMS/springboot-backend/src/main/java/com/rcc/lms/config/AppConfig.java
@@ -1,4 +1,22 @@
 package com.rcc.lms.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * General application configuration.
+ * Used for shared beans like PasswordEncoder.
+ */
+@Configuration
 public class AppConfig {
+
+    /**
+     * Password encoder used for hashing passwords before saving to database.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
