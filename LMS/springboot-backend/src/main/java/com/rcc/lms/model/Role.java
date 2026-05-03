9d5574bf0d900532dee469c8994cd3501a90f282
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-03 11:57:17 +0530

    Update Role.java

--- LMS/springboot-backend/src/main/java/com/rcc/lms/model/Role.java
+++ LMS/springboot-backend/src/main/java/com/rcc/lms/model/Role.java
@@ -1,4 +1,16 @@
 package com.rcc.lms.model;
 
-public class Role {
+/**
+ * System Roles for LMS
+ * This ensures only valid roles are used in the system
+ */
+public enum Role {
+    ADMIN,
+    STUDENT,
+    TEACHER,
+    TECHNICAL_OFFICER,
+
+    // Teacher sub-roles
+    HOD,
+    DEPUTY_PRINCIPAL
 }
