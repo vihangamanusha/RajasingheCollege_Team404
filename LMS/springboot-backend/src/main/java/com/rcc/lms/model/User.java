422e35b4098d8d83e879249e29790c6ed339b50b
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-03 10:21:09 +0530

    Update User.java

--- LMS/springboot-backend/src/main/java/com/rcc/lms/model/User.java
+++ LMS/springboot-backend/src/main/java/com/rcc/lms/model/User.java
@@ -1,4 +1,58 @@
 package com.rcc.lms.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * USER MODEL
+ * This represents a document in MongoDB
+ */
+
+@Document(collection = "users")
 public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String role;
+
+    public User() {}
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
