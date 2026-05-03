64d6cf42aa88de7663815c7e7d7952d76a7a018c
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-03 10:25:51 +0530

    Update UserRepository.java

--- LMS/springboot-backend/src/main/java/com/rcc/lms/repository/UserRepository.java
+++ LMS/springboot-backend/src/main/java/com/rcc/lms/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package com.rcc.lms.repository;
 
-public interface UserRepository {
+import com.rcc.lms.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+/**
+ * USER REPOSITORY
+ * Handles database operations
+ */
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 }
