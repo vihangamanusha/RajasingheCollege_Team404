ae0edb8ee3cb244024833e99ddc7c940a08474ad
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-05 01:28:41 +0530

    update

--- RCC-website/src/main.jsx
+++ RCC-website/src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import { router } from "./routes.jsx";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <LanguageProvider>
+    <RouterProvider router={router} />
+  </LanguageProvider>
+);
