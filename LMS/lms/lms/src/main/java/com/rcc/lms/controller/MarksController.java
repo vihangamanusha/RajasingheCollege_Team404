d2158fbb246c716ed608df552c05e75dd6df0c9e
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2026-05-06 16:34:19 +0530

    Update MarksController.java
    Update MarksController

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/MarksController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/MarksController.java
@@ -1,4 +1,23 @@
 package com.rcc.lms.controller;
 
+import com.rcc.lms.dto.MarksDTO;
+import com.rcc.lms.service.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/marks")
+@CrossOrigin
 public class MarksController {
+
+    @Autowired
+    private MarksService marksService;
+
+    @PostMapping("/save")
+    public String saveMarks(@RequestBody List<MarksDTO> marks) {
+        marksService.saveMarks(marks);
+        return "Marks saved successfully";
+    }
 }
