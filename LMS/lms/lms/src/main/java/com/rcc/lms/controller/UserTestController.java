0a5a1faac88e12c4c0d602b0d50ef633a0df13d1
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 12:43:36 +0530

    Update UserTestController.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserTestController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserTestController.java
@@ -1,11 +1,13 @@
 package com.rcc.lms.controller;
 
+import com.rcc.lms.dto.LoginRequest;
 import com.rcc.lms.entity.User;
 import com.rcc.lms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -14,6 +16,7 @@ public class UserTestController {
     @Autowired
     private UserRepository userRepository;
 
+    // ================= ADD USER =================
     @PostMapping("/add")
     public String addUser() {
 
@@ -29,5 +32,27 @@ public class UserTestController {
         userRepository.save(user);
 
         return "User saved successfully!";
+    }
+
+    // ================= LOGIN USER =================
+    @PostMapping("/login")
+    public String loginUser(@RequestBody LoginRequest request) {
+
+        Optional<User> userOptional =
+                userRepository.findByUsername(request.getUsername());
+
+        if (userOptional.isPresent()) {
+
+            User user = userOptional.get();
+
+            if (user.getPassword().equals(request.getPassword())) {
+                return "Login successful!";
+            } else {
+                return "Invalid password!";
+            }
+
+        } else {
+            return "User not found!";
+        }
     }
 }
