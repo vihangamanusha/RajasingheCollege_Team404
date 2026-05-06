9446ac51c098c56629300632d3b3a774543781bc
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 13:31:52 +0530

    Update UserController.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
-public class UserTestController {
+public class UserController {
 
     @Autowired
     private UserService userService;
