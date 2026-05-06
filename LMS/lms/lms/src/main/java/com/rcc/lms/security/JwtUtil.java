bb5ccebff31e34ca4f76b50e8571911db1656c49
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 14:53:10 +0530

    Update JwtUtil.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/security/JwtUtil.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/security/JwtUtil.java
@@ -2,22 +2,32 @@ package com.rcc.lms.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final String SECRET_KEY = "lms_secret_key_123";
+    // MUST be Base64 safe key (important)
+    private static final String SECRET_KEY =
+            "bXktbG1zLXNlY3JldC1rZXktMTIzNDU2Nzg5MDEyMzQ1Ng==";
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String generateToken(String username) {
 
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 }
