24c40874620f729ffc3e80c56cd7e87e794a3da2
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 13:06:48 +0530

    Update UserTestController.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserTestController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserTestController.java
@@ -2,47 +2,24 @@ package com.rcc.lms.controller;
 
 import com.rcc.lms.dto.LoginRequest;
 import com.rcc.lms.entity.User;
-import com.rcc.lms.repository.UserRepository;
+import com.rcc.lms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserTestController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
-    // ================= ADD USER =================
     @PostMapping("/add")
     public String addUser(@RequestBody User user) {
-
-        user.setCreatedDate(LocalDate.now());
-        user.setStatus("ACTIVE");
-
-        userRepository.save(user);
-
-        return "User registered successfully!";
+        return userService.registerUser(user);
     }
 
-    // ================= LOGIN USER =================
     @PostMapping("/login")
     public String loginUser(@RequestBody LoginRequest request) {
-
-        User user = userRepository.findByUsername(request.getUsername())
-                .orElse(null);
-
-        if (user == null) {
-            return "User not found!";
-        }
-
-        if (!user.getPassword().equals(request.getPassword())) {
-            return "Invalid password!";
-        }
-
-        return "Login successful! Role: " + user.getRole();
+        return userService.loginUser(request);
     }
 }
