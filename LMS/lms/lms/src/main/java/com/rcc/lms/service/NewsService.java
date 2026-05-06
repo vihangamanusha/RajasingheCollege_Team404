47f4b16392204ed8cd3982a953c419072ffe0f85
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-07 02:50:32 +0530

    update newsService.java
    add al news and create news

--- LMS/lms/lms/src/main/java/com/rcc/lms/service/NewsService.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/service/NewsService.java
@@ -1,7 +1,29 @@
 package com.rcc.lms.service;
 
+import com.rcc.lms.entity.News;
+import com.rcc.lms.repository.NewsRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NewsService {
+
+    private final NewsRepository newsRepository;
+
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    //get all news
+    public List<News> getALLNews(){
+        return newsRepository.findAll();
+    }
+
+    //create news
+    public News saveNews(News news){
+        return newsRepository.save(news);
+    }
+
+    
 }
