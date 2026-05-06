ca4f27905225e30a18084adcc3005c5e0165bb25
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 15:46:02 +0530

    Update AdminController.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/AdminController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/AdminController.java
@@ -1,4 +1,15 @@
 package com.rcc.lms.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin")
 public class AdminController {
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/test")
+    public String adminTest() {
+        return " Admin Access Granted";
+    }
 }
