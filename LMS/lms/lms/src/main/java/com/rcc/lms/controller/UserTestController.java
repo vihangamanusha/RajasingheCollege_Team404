6fdfe2eb9b7d0d04f89d637579c78c9279dc1b11
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 12:15:01 +0530

    Update UserTestController.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserTestController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/UserTestController.java
@@ -1,4 +1,33 @@
 package com.rcc.lms.controller;
 
+import com.rcc.lms.entity.User;
+import com.rcc.lms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/user")
 public class UserTestController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/add")
+    public String addUser() {
+
+        User user = new User();
+        user.setUserId("U001");
+        user.setUsername("admin");
+        user.setPassword("1234");
+        user.setRole("Admin");
+        user.setEmail("admin@gmail.com");
+        user.setCreatedDate(LocalDate.now());
+        user.setStatus("ACTIVE");
+
+        userRepository.save(user);
+
+        return "User saved successfully!";
+    }
 }
