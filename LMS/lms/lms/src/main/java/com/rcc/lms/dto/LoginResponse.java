4a30b08be447ff527e92ed01896f67c52a9dadc6
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 14:09:11 +0530

    Update LoginResponse.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/dto/LoginResponse.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/dto/LoginResponse.java
@@ -1,4 +1,40 @@
 package com.rcc.lms.dto;
 
 public class LoginResponse {
+
+    private String message;
+    private String username;
+    private String role;
+
+    public LoginResponse() {}
+
+    public LoginResponse(String message, String username, String role) {
+        this.message = message;
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
