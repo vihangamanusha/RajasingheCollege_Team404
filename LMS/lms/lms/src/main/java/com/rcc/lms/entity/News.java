5d928e035dc865c99ab5445a3405d3633a8eead6
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-07 01:36:55 +0530

    add filed names in news.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/entity/News.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/entity/News.java
@@ -1,4 +1,28 @@
 package com.rcc.lms.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)//column cannot null in db
+    @NotBlank//validating before saving
+    private String title;
+
+    @Column(nullable=false)
+    @NotBlank
+    private String content;
+
+    @Column(nullable=false)
+    private String date;
+
+    @Column(nullable=false)
+    private String image;
+
+
+
 }
