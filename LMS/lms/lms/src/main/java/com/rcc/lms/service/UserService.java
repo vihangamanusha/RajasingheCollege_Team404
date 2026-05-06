cd08436e5ad3edb16d6fd329a33526d31040f4a0
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-06 13:06:18 +0530

    Update UserService.java

--- LMS/lms/lms/src/main/java/com/rcc/lms/service/UserService.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/service/UserService.java
@@ -1,4 +1,43 @@
 package com.rcc.lms.service;
 
+import com.rcc.lms.dto.LoginRequest;
+import com.rcc.lms.entity.User;
+import com.rcc.lms.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // REGISTER USER
+    public String registerUser(User user) {
+        user.setCreatedDate(LocalDate.now());
+        user.setStatus("ACTIVE");
+
+        userRepository.save(user);
+
+        return "User registered successfully!";
+    }
+
+    // LOGIN USER
+    public String loginUser(LoginRequest request) {
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElse(null);
+
+        if (user == null) {
+            return "User not found!";
+        }
+
+        if (!user.getPassword().equals(request.getPassword())) {
+            return "Invalid password!";
+        }
+
+        return "Login successful! Role: " + user.getRole();
+    }
 }
