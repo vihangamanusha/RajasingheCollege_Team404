80eabcb23e4d0f814ca87e2cfc280659eb86d768
Nimesh Sanka Lakmal <nimeshsanka1209@gmail.com>
2026-05-07 17:49:43 +0530

    Update api.js

--- LMS/frontend/src/services/api.js
+++ LMS/frontend/src/services/api.js
@@ -4,5 +4,22 @@ const API = axios.create({
     baseURL: "http://localhost:8080",
 });
 
+// ======================
+// LOGIN API
+// ======================
 export const loginUser = (data) => API.post("/user/login", data);
-//export const registerUser = (data) => API.post("/auth/register", data);
+
+// ======================
+// ATTACH TOKEN AUTOMATICALLY
+// ======================
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export default API;
