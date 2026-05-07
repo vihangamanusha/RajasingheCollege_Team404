0414458715be84353cdad48be059a167302726fc
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2026-05-08 00:31:30 +0530

    update

--- LMS/lms/lms/src/main/java/com/rcc/lms/repository/MarksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rcc.lms.repository;
-
-import com.rcc.lms.entity.Marks;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarksRepository extends JpaRepository<Marks, Integer> {
-}
---
+++ LMS/lms/lms/src/main/java/com/rcc/lms/repository/TeacherMarksRepository.java
@@ -0,0 +1,7 @@
+package com.rcc.lms.repository;
+
+import com.rcc.lms.entity.TeacherMarks;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherMarksRepository extends JpaRepository<TeacherMarks, Integer> {
+}
