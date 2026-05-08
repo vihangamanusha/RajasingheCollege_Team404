b991c9b27fe5e995d24119a602e4355841e7b02d
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2026-05-08 08:50:05 +0530

    Update App.jsx

--- LMS/frontend/src/App.jsx
+++ LMS/frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import TeacherDashboard from "./pages/TeacherDashboard";
+import AddMarks from "./Component/TeacherAddMarks";
 
 function App() {
-  return (
-      <div>
-        <Login />
-      </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/TeacherDashboard" element={<TeacherDashboard />} />
+            <Route path="/TeacherAddMarks" element={<AddMarks />} />
+        </Routes>
+    );
 }
 
 export default App;
