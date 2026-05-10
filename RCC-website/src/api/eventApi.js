01521f0783696f7eef174867aaac2a35970abfa3
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 02:38:51 +0530

    create and update eventApi

---
+++ RCC-website/src/api/eventApi.js
@@ -0,0 +1,17 @@
+const BASE_URL = "http://localhost:8080/api/events";
+
+// get all events
+export const getAllEvents = async () => {
+  try {
+    const res = await fetch(BASE_URL);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch events");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching events:", error);
+    return [];
+  }
+};
