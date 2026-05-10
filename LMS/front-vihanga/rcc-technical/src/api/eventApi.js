7bd93736678a14dcf8c4ab06a450094ebc258d9b
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 02:10:45 +0530

    create and add eventApi.js

---
+++ LMS/front-vihanga/rcc-technical/src/api/eventApi.js
@@ -0,0 +1,41 @@
+const BASE_URL = "http://localhost:8080/api/events";
+
+//get all events
+export const getAllEvents = async () => {
+    const res=await fetch(BASE_URL);
+    return res.json();
+
+};
+
+
+//add event
+export const addEvent = async (eventData) => {
+    const res=await fetch(BASE_URL,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(eventData)
+    });
+    return res.json();
+};
+
+//update event
+export const updateEvent = async (Id, eventData) => {
+    const res=await fetch(`${BASE_URL}/${Id}`,{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(eventData)
+    });
+    return res.json();
+};
+
+//delete event
+export const deleteEvent = async (Id) => {
+    const res=await fetch(`${BASE_URL}/${Id}`,{
+        method:"DELETE"
+    });
+    return res.json();
+}
