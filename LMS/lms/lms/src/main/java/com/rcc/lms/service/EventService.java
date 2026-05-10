e31adbc68201bc78e06fdda867e67a1357d16e71
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-10 23:26:37 +0530

    update eventService
    add,delete,save,update

--- LMS/lms/lms/src/main/java/com/rcc/lms/service/EventService.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/service/EventService.java
@@ -1,4 +1,47 @@
 package com.rcc.lms.service;
 
+import com.rcc.lms.entity.Event;
+import com.rcc.lms.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EventService {
+    private final EventRepository repository;
+
+    public EventService(EventRepository repository) {
+        this.repository = repository;
+    }
+
+    //get all events
+    public List<Event> getAllEvents() {
+        return repository.findAll();
+    }
+
+    //add events
+    public Event addEvent(Event event){
+        return repository.save(event);
+    }
+
+    //update events
+    public Event updateEvent(Long id, Event updatedEvent) {
+
+        Event event = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Event not found"));
+
+        event.setTopic(updatedEvent.getTopic());
+        event.setDescription(updatedEvent.getDescription());
+        event.setDate(updatedEvent.getDate());
+        event.setTime(updatedEvent.getTime());
+        event.setVenue(updatedEvent.getVenue());
+
+        return repository.save(event);
+    }
+
+    //delet event
+    public Event deleteEvent(Long id){
+        repository.deleteById(id);
+    }
+
 }
