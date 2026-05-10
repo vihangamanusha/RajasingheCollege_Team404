2edee1aee684191652f8c058257afd7283600cc0
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 01:46:54 +0530

    update event controller

--- LMS/lms/lms/src/main/java/com/rcc/lms/controller/EventController.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/controller/EventController.java
@@ -1,4 +1,46 @@
 package com.rcc.lms.controller;
 
+import com.rcc.lms.entity.Event;
+import com.rcc.lms.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+@CrossOrigin(origins = "*")
 public class EventController {
+
+    private final EventService service;
+
+    public EventController(EventService service) {
+        this.service = service;
+    }
+
+
+
+    //get all events
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return service.getAllEvents();
+    }
+
+    //add events
+    @PostMapping
+    public Event addEvent(@RequestBody Event event){
+        return service.addEvent(event);
+    }
+
+    //update event
+    @PutMapping("/{id}")
+    public Event updateEvent(@PathVariable Long id, @RequestBody Event event){
+        return service.updateEvent(id, event);
+    }
+
+    //delete event
+    public void deleteEvent(@PathVariable Long id){
+        service.deleteEvent(id);
+    }
+
+
 }
