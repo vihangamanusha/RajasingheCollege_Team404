34ba5db26926e74ecfbc7ef1671db0a6f1e8cc96
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 18:28:32 +0530

    update announcement.java
    add field and getter and setter

--- LMS/lms/lms/src/main/java/com/rcc/lms/entity/Announcement.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/entity/Announcement.java
@@ -1,4 +1,73 @@
 package com.rcc.lms.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="announcements")
 public class Announcement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String category;
+    private String targetAudeience;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Announcement() {
+        this.createdAt = LocalDateTime.now();
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTargetAudeience() {
+        return targetAudeience;
+    }
+
+    public void setTargetAudeience(String targetAudeience) {
+        this.targetAudeience = targetAudeience;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
