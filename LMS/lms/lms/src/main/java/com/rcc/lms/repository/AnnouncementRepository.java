e61b403c9b1eb4bc66cd4dc3cfa965bb418db2bb
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 18:33:07 +0530

    update AnnouncementRepository etends Jparepositroy

--- LMS/lms/lms/src/main/java/com/rcc/lms/repository/AnnouncementRepository.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/repository/AnnouncementRepository.java
@@ -1,4 +1,8 @@
 package com.rcc.lms.repository;
 
-public interface AnnouncementRepository {
+import com.rcc.lms.entity.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 }
