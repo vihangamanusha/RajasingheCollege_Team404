49f6b4cc24de1fb3396cc55b2ba86fb5bb444559
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 13:18:41 +0530

    update main.jsx

--- RCC-website/src/main.jsx
+++ RCC-website/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { router } from "./routes.jsx";
-import AllStaff from "./pages/academic/AllStaff";
+import AllStaff from "./pages/AllStaff";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
