484f126ba4a06627ee6ef0b51dc621ae5c0d4768
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-11 18:19:53 +0530

    create annoucment.java

--- RCC-website/src/main.jsx
+++ RCC-website/src/main.jsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { router } from "./routes.jsx";
-import AllStaff from "./pages/AllStaff";
+/*import AllStaff from "./pages/AllStaff";*/
+
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
