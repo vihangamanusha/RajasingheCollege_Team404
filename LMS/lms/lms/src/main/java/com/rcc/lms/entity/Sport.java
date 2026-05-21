e6257dc2e8048e2b203b882d2f965cd1dc9da6e5
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-21 11:56:06 +0530

    create sport.java entity

---
+++ LMS/lms/lms/src/main/java/com/rcc/lms/entity/Sport.java
@@ -0,0 +1,70 @@
+package com.rcc.lms.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="sports")
+public class Sport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String typesport;
+
+    @Column(nullable = false)
+    private String topic;
+
+    private String description;
+    private String image;
+
+    public Sport(){}
+
+    public Sport(long id, String typesport, String topic, String description, String image) {
+        this.id = id;
+        this.typesport = typesport;
+        this.topic = topic;
+        this.description = description;
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTypesport() {
+        return typesport;
+    }
+
+    public void setTypesport(String typesport) {
+        this.typesport = typesport;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
