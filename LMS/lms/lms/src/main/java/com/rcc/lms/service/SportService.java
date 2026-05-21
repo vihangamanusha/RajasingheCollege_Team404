9a3ca95dc60ec8af0d3a499cffa0458064ccfb1c
vihangamanusha <vihangamanusha07@gmail.com>
2026-05-21 12:03:00 +0530

    complete sportservice

--- LMS/lms/lms/src/main/java/com/rcc/lms/service/SportService.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/service/SportService.java
@@ -1,4 +1,42 @@
 package com.rcc.lms.service;
 
+import com.rcc.lms.entity.Sport;
+import com.rcc.lms.repository.SportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class SportService {
+    @Autowired
+    private SportRepository repository;
+
+    public List<Sport> getAllSports() {
+        return repository.findAll();
+    }
+
+    public Sport getSportById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Sport> getByType(String typeSport) {
+        return repository.findByTypeSport(typeSport);
+    }
+
+    public Sport saveSport(Sport sport) {
+        return repository.save(sport);
+    }
+
+    public Sport updateSport(Long id, Sport updatedSport) {
+        return repository.findById(id).map(sport -> {
+            sport.setTypesport(updatedSport.getTypesport());
+            sport.setTopic(updatedSport.getTopic());
+            sport.setImage(updatedSport.getImage());
+            sport.setDescription(updatedSport.getDescription());
+            return repository.save(sport);
+        }).orElse(null);
+    }
+
+    public void deleteSport(Long id) {
+        repository.deleteById(id);
+    }
 }
