f9226b15d598a7191384a2a0adc7099277240d66
vihangamanusha <vihangamanusha07@gmail.com>
2026-06-03 03:06:28 +0530

    update students.java
    add new attribute

--- LMS/lms/lms/src/main/java/com/rcc/lms/entity/Student.java
+++ LMS/lms/lms/src/main/java/com/rcc/lms/entity/Student.java
@@ -28,13 +28,23 @@ public class Student {
 
     private String dob;
 
-
-
-    private String gname;
-
-    private String contactNo;
-
     private String address;
+
+    private String gardian_name;
+
+    private String gardian_contact;
+
+    private String emergency_contact_name_01;
+    private String emergency_contact_name_02;
+
+    private String emergency_contact_contact_01;
+    private String emergency_contact_contact_02;
+
+    private String mother_job;
+
+    private String father_job;
+
+    private String donation;
 
     public Long getUserId() {
         return userId;
@@ -68,20 +78,20 @@ public class Student {
         this.password = password;
     }
 
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
     public String getStudentClass() {
         return studentClass;
     }
 
     public void setStudentClass(String studentClass) {
         this.studentClass = studentClass;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
     }
 
     public String getMedium() {
@@ -92,12 +102,12 @@ public class Student {
         this.medium = medium;
     }
 
-    public String getContactNo() {
-        return contactNo;
+    public String getDob() {
+        return dob;
     }
 
-    public void setContactNo(String contactNo) {
-        this.contactNo = contactNo;
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 
     public String getAddress() {
@@ -108,21 +118,75 @@ public class Student {
         this.address = address;
     }
 
-    public String getGrade() {
-        return grade;
+    public String getGardian_name() {
+        return gardian_name;
     }
 
-    public void setGrade(String grade) {
-        this.grade = grade;
+    public void setGardian_name(String gardian_name) {
+        this.gardian_name = gardian_name;
     }
 
-
-
-    public String getGname() {
-        return gname;
+    public String getGardian_contact() {
+        return gardian_contact;
     }
 
-    public void setGname(String gname) {
-        this.gname = gname;
+    public void setGardian_contact(String gardian_contact) {
+        this.gardian_contact = gardian_contact;
+    }
+
+    public String getEmergency_contact_name_01() {
+        return emergency_contact_name_01;
+    }
+
+    public void setEmergency_contact_name_01(String emergency_contact_name_01) {
+        this.emergency_contact_name_01 = emergency_contact_name_01;
+    }
+
+    public String getEmergency_contact_name_02() {
+        return emergency_contact_name_02;
+    }
+
+    public void setEmergency_contact_name_02(String emergency_contact_name_02) {
+        this.emergency_contact_name_02 = emergency_contact_name_02;
+    }
+
+    public String getEmergency_contact_contact_01() {
+        return emergency_contact_contact_01;
+    }
+
+    public void setEmergency_contact_contact_01(String emergency_contact_contact_01) {
+        this.emergency_contact_contact_01 = emergency_contact_contact_01;
+    }
+
+    public String getEmergency_contact_contact_02() {
+        return emergency_contact_contact_02;
+    }
+
+    public void setEmergency_contact_contact_02(String emergency_contact_contact_02) {
+        this.emergency_contact_contact_02 = emergency_contact_contact_02;
+    }
+
+    public String getMother_job() {
+        return mother_job;
+    }
+
+    public void setMother_job(String mother_job) {
+        this.mother_job = mother_job;
+    }
+
+    public String getFather_job() {
+        return father_job;
+    }
+
+    public void setFather_job(String father_job) {
+        this.father_job = father_job;
+    }
+
+    public String getDonation() {
+        return donation;
+    }
+
+    public void setDonation(String donation) {
+        this.donation = donation;
     }
 }
