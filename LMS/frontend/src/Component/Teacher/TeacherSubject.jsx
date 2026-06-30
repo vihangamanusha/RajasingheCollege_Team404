0524fe02f2085cfc43564528e5593621c85e9c42
wasantha2000 <www.wasanthakeerthi9@gmail.com>
2026-06-30 20:13:23 +0530

    Create TeacherSubject.jsx

---
+++ LMS/frontend/src/Component/Teacher/TeacherSubject.jsx
@@ -0,0 +1,19 @@
+import TeacherSidebar from "./TeacherSidebar";
+
+function Subjects() {
+    return (
+        <div style={{ display: "flex" }}>
+
+            {/*<TeacherSidebar />  */}
+
+            <div style={{ padding: "30px", width: "100%" }}>
+                <h2>My Subjects</h2>
+
+                <p>Here you can manage your subjects.</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Subjects;
